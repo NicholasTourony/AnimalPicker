4a7096945cbcfda8e93cd0404c15a6b5690c69e4
NicholasTourony <natourony@quinnipiac.edu>
2024-02-27 15:27:10 -0500

    Continuously prompts user until they enter a correct input

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,19 +4,27 @@ public class AnimalPicker
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Would you like to see a dog or a cat: ");
-        String answer = scanner.nextLine();
-        if (answer.equals("dog"))
+        boolean correctInput = true;
+        do
         {
-            System.out.println("DOG ASCII ART HERE");
-        }
-        else if (answer.equals("cat"))
-        {
-            System.out.println("CAT ASCII ART HERE");
-        }
-        else
-        {
-            System.out.println("Error: you must enter \"dog\" or \"cat\"");
-        }
+            System.out.print("Would you like to see a dog or a cat: ");
+            String answer = scanner.nextLine();
+
+            if (answer.equals("dog"))
+            {
+                System.out.println("DOG ASCII ART HERE");
+                correctInput = true;
+            }
+            else if (answer.equals("cat"))
+            {
+                System.out.println("CAT ASCII ART HERE");
+                correctInput = true;
+            }
+            else
+            {
+                System.out.println("Error: you must enter \"dog\" or \"cat\"");
+                correctInput = false;
+            }
+        } while (!correctInput);
     }
 }
